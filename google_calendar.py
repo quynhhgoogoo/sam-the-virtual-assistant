19f3d7f84558e63b26856d6e2468fb0231c42536
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-06-21 22:44:58 +0300

    get date

--- google_calendar.py
+++ google_calendar.py
@@ -20,16 +20,15 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-#from bot import *
+from bot import *
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-
+DAY_EXTENSIONS = ['st', 'nd', 'rd']
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',]
+MONTHS = ['january','february','march','april','may','june','july','august','september','october','november','december']
 
 def authenticate_google():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -68,6 +67,59 @@ def get_calendar_events(e, service):
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
+def get_date(text):
+    text = text.lower()
+    #Get today's date
+    today = datetime.date.today()
+    #Break up text into different words
+    if text.count("today")>0:
+        return today
 
-service = authenticate_google()
-get_calendar_events(10, service)
+    #Initialize variables for day
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    #Start iterating through voice's input to find keywords for date, month and year
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word)+1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENSIONS:
+                found = word.find(ext)
+                if found>0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    #When given month is less than today's month
+    if month < today.month and month !=-1:
+        year = year + 1
+
+    #When day is less than today and month is not defined
+    if day < today.day and day != -1 and month ==-1:
+        month = month+1
+
+    #When only day of weeks is given
+    if month == -1 and day == -1 and day_of_week != -1:
+        current_day_of_week = today.weekday()   #Mon-Sun/0-6
+        dif = day_of_week - current_day_of_week
+        if dif<0:
+            dif+=7      #Move to same day of week on next week
+            if text.count("next") >=1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+    return datetime.date(month=month, day=day, year=year)
+
+#service = authenticate_google()
+#get_calendar_events(10, service)
+
+text = bot_ear().lower()
+print(get_date(text))
